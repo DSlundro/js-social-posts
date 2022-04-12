825b1f76d60f2ec9492406d2283749acd8af6ac1
DSlundro <dragan.savic95@outlook.it>
2022-04-12 15:03:31 +0200

    initial commit + milestone 1

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+// Milestone 1
+// Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+//      id del post, numero progressivo da 1 a n
+//      nome autore,
+//      foto autore,
+//      data in formato americano (mm-gg-yyyy),
+//      testo del post,
+//      immagine (non tutti i post devono avere una immagine),
+//      numero di likes.
+
+
+// creo un array con tutti gli oggetti e infomraioni
+const objects = [
+    {
+        id: '1',
+        autore: {
+                nome: 'Luffy',
+                foto: 'https://unsplash.it/300/300?image=',
+                },
+        data: '04-12-2022',
+        testo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam necessitatibus earum magni cumque, possimus incidunt.',
+        img: 'https://unsplash.it/300/300?image=',
+        likes: '486'
+    },
+    {
+        id: '2',
+        autore: {
+                nome: 'Zoro',
+                foto: 'https://unsplash.it/300/300?image=',
+                },
+        data: '04-12-2022',
+        testo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam necessitatibus earum magni cumque, possimus incidunt.',
+        img: 'https://unsplash.it/300/300?image=',
+        likes: '1285'
+    },
+    {
+        id: '3',
+        autore: {
+                nome: 'Nami',
+                foto: 'https://unsplash.it/300/300?image=',
+                },
+        data: '04-12-2022',
+        testo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam necessitatibus earum magni cumque, possimus incidunt.',
+        img: 'https://unsplash.it/300/300?image=',
+        likes: '739'
+    },
+]
+
+
+
+
